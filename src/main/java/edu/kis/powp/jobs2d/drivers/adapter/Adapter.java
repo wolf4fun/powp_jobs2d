f1ba80eacaf3220b62ddf254ce7712d0f67f0f12
wolf4fun <wilczek4fun@gmail.com>
2021-03-09 00:24:01 +0100

    3.3.1 adding LineDrawerAdapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Adapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Adapter.java
@@ -8,7 +8,7 @@ import static edu.kis.powp.jobs2d.features.DrawerFeature.getDrawerController;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class Adapter extends DrawPanelController implements Job2dDriver {
+public class Adapter extends DrawPanelController implements Job2dDriver{
 	private int startX = 0, startY = 0;
 
 	public Adapter() {
@@ -33,6 +33,6 @@ public class Adapter extends DrawPanelController implements Job2dDriver {
 
 	@Override
 	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+		return "Basic Adapter";
 	}
 }
---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -0,0 +1,39 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.LineFactory;
+
+public class LineDrawerAdapter extends Adapter {
+
+  private LineType lineType = LineType.DOTTED;
+
+  public LineDrawerAdapter() {
+		super();
+	}
+
+  enum LineType {
+    BASIC, DOTTED, SPECIAL
+  }
+
+  public void setLineType(LineType type) {
+    this.lineType = type;
+  }
+
+  private ILine getLine() {
+    switch (this.lineType) {
+      case BASIC:
+        return LineFactory.getBasicLine();
+      case DOTTED:
+        return LineFactory.getDottedLine();
+      case SPECIAL:
+        return LineFactory.getSpecialLine();
+      default:
+        return LineFactory.getBasicLine();
+    }
+  }
+
+	@Override
+	public String toString() {
+		return "Extended Adapter";
+	}
+}
